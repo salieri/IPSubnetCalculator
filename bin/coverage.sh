0351d218c2b20fc7c2bcff08475ae51fac7ed097
Aleksi Asikainen <aleksi.asikainen@solera.com>
2016-05-13 18:47:39 -0500

    Cleaner coverage hook

--- bin/coverage.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js --verbose
